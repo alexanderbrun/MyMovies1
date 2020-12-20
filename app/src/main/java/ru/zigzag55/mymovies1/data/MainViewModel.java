6af75efdfe8a4a6d6cbaf9cce10b0a93789f9658
alexanderbrun <alhimiq7@gmail.com>
2020-12-20 14:17:53 +0600

    add database class object, add ViewModel

---
+++ app/src/main/java/ru/zigzag55/mymovies1/data/MainViewModel.java
@@ -0,0 +1,92 @@
+package ru.zigzag55.mymovies1.data;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private static MovieDatabase database;
+    private LiveData<List<Movie>> movies;
+
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        database = MovieDatabase.getInstance(getApplication());
+        movies = database.movieDao().getAllMovies();
+    }
+
+    public Movie getMovieById(int id) {
+        try {
+            return new GetMovieTask().execute(id).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static class GetMovieTask extends AsyncTask<Integer, Void, Movie> {
+
+        @Override
+        protected Movie doInBackground(Integer... integers) {
+            if (integers != null && integers.length > 0) {
+                return database.movieDao().getMovieById(integers[0]);
+            }
+            return null;
+        }
+    }
+
+    public void deleteAllMovies() {
+        new DeleteAllTask().execute();
+    }
+
+    private static class DeleteAllTask extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            database.movieDao().deleteAllMovies();
+            return null;
+        }
+    }
+
+    public void insertMovie(Movie movie) {
+        new InsertMovieTask().execute(movie);
+    }
+
+    private static class InsertMovieTask extends AsyncTask<Movie, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            if (movies != null && movies.length > 0){
+                database.movieDao().insertMovie(movies[0]);
+            }
+            return null;
+        }
+    }
+
+    public void deleteMovie(Movie movie) {
+        new DeleteMovieTask().execute(movie);
+    }
+
+    private static class DeleteMovieTask extends AsyncTask<Movie, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            if (movies != null && movies.length > 0){
+                database.movieDao().deleteMovie(movies[0]);
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/ru/zigzag55/mymovies1/data/MovieDatabase.java
@@ -0,0 +1,26 @@
+package ru.zigzag55.mymovies1.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
+public abstract class MovieDatabase extends RoomDatabase {
+    private static final String MOVIES_DB = "movies.db";
+    private static MovieDatabase database;
+    private static final Object LOCK = new Object();
+
+    public static MovieDatabase getInstance(Context context) {
+        synchronized (LOCK) {
+            if (database == null) {
+                database = Room.databaseBuilder(context, MovieDatabase.class, MOVIES_DB).build();
+            }
+            return database;
+        }
+
+    }
+
+    public abstract MovieDao movieDao();
+}
